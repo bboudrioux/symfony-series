86bce2dda0de9cd92192ad053e1ebbffcc8bab43
Brian Boudrioux <bboudrioux@gmail.com>
2025-07-21 12:46:35 +0200

    add program page

---
+++ src/Controller/ProgramController.php
@@ -0,0 +1,19 @@
+<?php
+
+// src/Controller/ProgramController.php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ProgramController extends AbstractController {
+
+    #[Route('/', name: 'program_index')]
+    public function index(): Response {
+
+        return $this->render('program/index.html.twig', ["title" => "Program page"]);
+
+    }
+}
